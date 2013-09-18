ffe3892269f7bbb5ddaee4cafb4310c777fb2324
Deep Shah <deep5187@gmail.com>
2013-09-18 14:58:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learnaptui::Application.config.secret_key_base = 'f001360d531ecfe23d0e140e3b30bd9a065bd50bda72d6e2682e52b7cc5b50e52b245ab04e530b971ae64ff2c46f81519920fc5b95a773d4d709afada0e8104b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Learnaptui::Application.config.session_store :cookie_store, key: '_learnaptui_session'
